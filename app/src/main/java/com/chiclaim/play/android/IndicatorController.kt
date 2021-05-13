edaefbabebdba27ef4c95835de9a270585134961
chiclaim <chiclaim@gmail.com>
2021-05-13 17:57:13 +0800

    [update] 封装 ActionBarIndicator 控制类

---
+++ app/src/main/java/com/chiclaim/play/android/IndicatorController.kt
@@ -0,0 +1,65 @@
+package com.chiclaim.play.android
+
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
+import androidx.annotation.StringRes
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.graphics.drawable.DrawerArrowDrawable
+
+/**
+ *
+ * @author by chiclaim@google.com
+ */
+class IndicatorController private constructor(val activity: AppCompatActivity) {
+
+
+    companion object {
+        fun of(activity: AppCompatActivity) = IndicatorController(activity)
+    }
+
+
+    private var arrowDrawable: DrawerArrowDrawable? = null
+    private var arrowAnimator: ValueAnimator? = null
+
+    fun setNavigationIcon(
+        drawable: DrawerArrowDrawable,
+        @StringRes desc: Int
+    ) {
+        activity.supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        val delegate: ActionBarDrawerToggle.Delegate? = activity.drawerToggleDelegate
+        delegate?.setActionBarUpIndicator(
+            drawable,
+            desc
+        )
+    }
+
+
+    fun setActionBarUpIndicator(showAsDrawerIndicator: Boolean) {
+        var animate = true
+        if (arrowDrawable == null) {
+            arrowDrawable = DrawerArrowDrawable(activity)
+            animate = false
+        }
+
+        val arrow = arrowDrawable ?: return
+
+        setNavigationIcon(
+            arrowDrawable!!,
+            if (showAsDrawerIndicator) R.string.nav_app_bar_open_drawer_description
+            else R.string.nav_app_bar_navigate_up_description
+        )
+        val endValue = if (showAsDrawerIndicator) 0f else 1f
+        if (animate) {
+            val startValue: Float = arrow.progress
+            arrowAnimator?.cancel()
+            arrowAnimator = ObjectAnimator.ofFloat(
+                arrow, "progress", startValue, endValue
+            )
+            arrowAnimator?.start()
+        } else {
+            arrow.progress = endValue
+        }
+    }
+
+}
--- app/src/main/java/com/chiclaim/play/android/MainActivity.kt
+++ app/src/main/java/com/chiclaim/play/android/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.chiclaim.play.android
 
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
+import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
-import androidx.appcompat.widget.Toolbar
+import android.util.Log
+import android.view.Gravity
+import android.view.MenuItem
+import android.view.View
+import androidx.annotation.StringRes
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.graphics.drawable.DrawerArrowDrawable
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.*
 import com.chiclaim.play.android.base.BaseActivity
+import com.chiclaim.play.android.funcs.user.login.LoginActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 
@@ -20,35 +29,85 @@ import com.google.android.material.navigation.NavigationView
 class MainActivity : BaseActivity() {
 
     private lateinit var navigationView: NavigationView
-    private lateinit var bottomNav: BottomNavigationView
-    private lateinit var navController: NavController
+    private lateinit var bottomNavigation: BottomNavigationView
     private lateinit var drawerLayout: DrawerLayout
+
+    private lateinit var navController: NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
+
+    private lateinit var indicatorController: IndicatorController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        navigationView = findViewById(R.id.navigation_view)
-        bottomNav = findViewById(R.id.bottom_navigation)
+
+
+        navigationView = findViewById(R.id.drawer_navigation_view)
+        bottomNavigation = findViewById(R.id.bottom_navigation)
+        drawerLayout = findViewById(R.id.drawer_layout)
+
+        indicatorController = IndicatorController.of(this)
+        indicatorController.setActionBarUpIndicator(true)
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.hostFragment) as NavHostFragment
         navController = navHostFragment.navController
 
-        setupBottomNavigation()
+        navigationView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.navigation_home -> {
+                    startActivity(Intent(this, LoginActivity::class.java))
+                }
+            }
+            return@setNavigationItemSelectedListener true
+        }
+
+        drawerLayout.addDrawerListener(object : DrawerLayout.DrawerListener {
+            override fun onDrawerSlide(drawerView: View, slideOffset: Float) {
+            }
+
+            override fun onDrawerOpened(drawerView: View) {
+                indicatorController.setActionBarUpIndicator(false)
+                Log.e("addDrawerListener", "onDrawerOpened")
+            }
+
+            override fun onDrawerClosed(drawerView: View) {
+                indicatorController.setActionBarUpIndicator(true)
+                Log.e("addDrawerListener", "onDrawerClosed")
+
+            }
+
+            override fun onDrawerStateChanged(newState: Int) {
+                Log.e("addDrawerListener", "onDrawerStateChanged:$newState")
+            }
+        })
+
+        bottomNavigation.setupWithNavController(navController)
 
 
-
-        drawerLayout = findViewById(R.id.drawer_layout)
-        // For Navigation UP
         appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
-        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
 
-        //NavigationUI.setupWithNavController(navigation_view,navController)
+        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+
+        // DrawerLayout 中的 NavigationView item 的点击和 BottomNavigationView 联动
         NavigationUI.setupWithNavController(navigationView, navController)
 
 
     }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                drawerLayout.closeDrawer(GravityCompat.START)
+            } else {
+                drawerLayout.openDrawer(GravityCompat.START)
+            }
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 
     override fun onSupportNavigateUp(): Boolean {
         //return navController.navigateUp()
@@ -56,7 +115,4 @@ class MainActivity : BaseActivity() {
 
     }
 
-    private fun setupBottomNavigation() {
-        bottomNav.setupWithNavController(navController)
-    }
 }
