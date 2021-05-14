31047e871a9e4fc7bfdefc09a44b0eaafbd20139
chiclaim <chiclaim@gmail.com>
2021-05-14 16:19:35 +0800

    [update] 使用 ActionBarDrawerToggle 重构代码

--- app/src/main/java/com/chiclaim/play/android/MainActivity.kt
+++ app/src/main/java/com/chiclaim/play/android/MainActivity.kt
@@ -1,17 +1,9 @@
 package com.chiclaim.play.android
 
-import android.animation.ObjectAnimator
-import android.animation.ValueAnimator
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.view.Gravity
-import android.view.MenuItem
-import android.view.View
-import androidx.annotation.StringRes
 import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.appcompat.graphics.drawable.DrawerArrowDrawable
-import androidx.core.view.GravityCompat
+import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
@@ -28,55 +20,38 @@ import com.google.android.material.navigation.NavigationView
  */
 class MainActivity : BaseActivity() {
 
-    private lateinit var navigationView: NavigationView
+    private lateinit var drawerNavigationView: NavigationView
     private lateinit var bottomNavigation: BottomNavigationView
     private lateinit var drawerLayout: DrawerLayout
+    private lateinit var toolbar: Toolbar
 
     private lateinit var navController: NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
 
-    private lateinit var indicatorController: IndicatorController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        navigationView = findViewById(R.id.drawer_navigation_view)
+        drawerNavigationView = findViewById(R.id.drawer_navigation_view)
         bottomNavigation = findViewById(R.id.bottom_navigation)
         drawerLayout = findViewById(R.id.drawer_layout)
-
-        indicatorController = IndicatorController.of(this)
-        indicatorController.setActionBarUpIndicator(true)
+        toolbar = findViewById(R.id.toolbar)
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.hostFragment) as NavHostFragment
         navController = navHostFragment.navController
 
-        navigationView.setNavigationItemSelectedListener {
-            when (it.itemId) {
-                R.id.navigation_home -> {
-                    startActivity(Intent(this, LoginActivity::class.java))
-                }
-            }
-            return@setNavigationItemSelectedListener true
-        }
 
-        drawerLayout.addDrawerListener(object : DrawerLayout.DrawerListener {
-            override fun onDrawerSlide(drawerView: View, slideOffset: Float) {
-            }
-
-            override fun onDrawerOpened(drawerView: View) {
-                indicatorController.setActionBarUpIndicator(false)
-            }
-
-            override fun onDrawerClosed(drawerView: View) {
-                indicatorController.setActionBarUpIndicator(true)
-            }
-
-            override fun onDrawerStateChanged(newState: Int) {
-            }
-        })
+        val actionBarDrawerToggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
 
         bottomNavigation.setupWithNavController(navController)
 
@@ -86,28 +61,29 @@ class MainActivity : BaseActivity() {
         //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
 
         // DrawerLayout 中的 NavigationView item 的点击和 BottomNavigationView 联动
-        NavigationUI.setupWithNavController(navigationView, navController)
+        //NavigationUI.setupWithNavController(drawerNavigationView, navController)
 
 
-    }
-
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == android.R.id.home) {
-            if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-                drawerLayout.closeDrawer(GravityCompat.START)
-            } else {
-                drawerLayout.openDrawer(GravityCompat.START)
+        drawerNavigationView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.navigation_home -> {
+                    startActivity(Intent(this, LoginActivity::class.java))
+                }
             }
-            return true
+            return@setNavigationItemSelectedListener true
         }
-        return super.onOptionsItemSelected(item)
+
+        bottomNavigation.setOnNavigationItemSelectedListener {
+            toolbar.title = it.title
+            true
+        }
+        // 触发 OnNavigationItemSelectedListener 更新 title
+        bottomNavigation.selectedItemId = R.id.navigation_home
     }
 
 
     override fun onSupportNavigateUp(): Boolean {
         return NavigationUI.navigateUp(navController, appBarConfiguration)
-
     }
 
 }
