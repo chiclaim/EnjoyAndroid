bbeae7766b5f21939181b9fec604fa6bb71655aa
chiclaim <chiclaim@gmail.com>
2021-05-27 16:58:29 +0800

    [add]封装 Fragment 懒加载功能

--- app/src/main/java/com/chiclaim/play/android/base/BaseFragment.kt
+++ app/src/main/java/com/chiclaim/play/android/base/BaseFragment.kt
@@ -5,10 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
-import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
-import com.chiclaim.play.android.databinding.FragmentHomeBinding
 
 /**
  * Fragment 基类。为了避免被直接使用，需要声明为 abstract
@@ -29,41 +27,56 @@ abstract class BaseFragment<T : ViewDataBinding> : Fragment() {
         ScopeViewModel.ApplicationViewModelProvider(requireActivity().application)
     }
 
-    private var viewDataBinding: T? = null
+    private var isLoaded = false
+    private var destroyViewState = false
 
     @LayoutRes
     abstract fun getLayoutId(): Int
 
     /**
-     * 在 [onCreateView] 方法中，构造 viewDataBinding 之后被调用
+     * 延迟加载数据。当 Fragment onResume 时候才会调用，可以在此方法中进行网络请求等操作
      */
-    open fun init() {
+    abstract fun lazyLoad()
 
-    }
+
+    /**
+     * 在 [onViewCreated] 方法中调用，可以做一些初始化操作
+     */
+    open fun init() {}
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        viewDataBinding = DataBindingUtil.inflate(
-            inflater,
-            getLayoutId(), container, false
-        )
+        return inflater.inflate(getLayoutId(), container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         init()
-        return viewDataBinding?.root
     }
 
 
-    fun requireDataBinding(): T {
-        if (viewDataBinding == null) error("must call after onCreateView()")
-        return viewDataBinding as T
+    override fun onResume() {
+        super.onResume()
+        if (!isLoaded && !isHidden) {
+            isLoaded = true
+            lazyLoad()
+        }
     }
-
 
     override fun onDestroyView() {
         super.onDestroyView()
-        viewDataBinding?.unbind()
+        isLoaded = false
+        destroyViewState = true
+    }
+
+    fun getDestroyViewStateAndReset(): Boolean {
+        val d = destroyViewState
+        destroyViewState = false
+        return d
     }
 
 
